22e9069f90ea4d15e18d380199ef64fc0037236a
Josiel Almeida de Souza <105735609+gunterbr@users.noreply.github.com>
2023-02-26 16:38:17 -0400

    Update criar_user.sh

--- criar_user.sh
+++ criar_user.sh
@@ -3,17 +3,8 @@
 
 echo "Criando usuários do sistema...."
 
-useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest10 -e
-
-useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest11 -e
-
-useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest12 -e
-
-useradd guest13 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest13 -e
+useradd root -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G root
+passwd root -e
 
 echo "Finalizado!!"
 
